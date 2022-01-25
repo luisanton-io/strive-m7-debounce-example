a79135525897fc66f60d7591cdf7c288736bdd16
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-01-25 18:02:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+  const [value, setValue] = useState('')
+  const [debouncedValue, setDebouncedValue] = useState('')
+
+
+  const [countdown, setCountdown] = useState(null)
+
+
+  useEffect(() => {
+    if (countdown) {
+      clearTimeout(countdown)
+    }
+
+    setCountdown(setTimeout(() => {
+      setDebouncedValue(value)
+    }, 300))
+
+  }, [value])
+
+  useEffect(() => {
+
+    if (!debouncedValue) return
+
+    console.log("Fetching data using value: ", debouncedValue);
+
+    // here you are supposed to fetch
+
+    (async () => {
+      const response = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?search=${debouncedValue}&limit=10`)
+      const data = await response.json()
+
+      console.log(data)
+    })()
+  }, [debouncedValue])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" className="" value={value} onChange={e => setValue(e.target.value)} />
+
     </div>
   );
 }
